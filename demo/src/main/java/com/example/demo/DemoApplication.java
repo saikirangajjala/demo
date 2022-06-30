b16927cb0abf65f1941b84c9d918d0a420588b2f
Saikiran <saikirangajjala000@gmail.com>
2022-06-30 11:44:06 -0500

    Initial commit

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+
+package com.example.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping(value = "/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+}
